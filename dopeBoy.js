d796195ad76ddb493d144dc9a96fe668a6c45927
Denny da Vjncj <daVjncjCode@gmail.com>
2020-12-21 11:40:41 -0500

    updated version

--- dopeBoy.js
+++ dopeBoy.js
@@ -4,7 +4,7 @@ const session=require('express-session');
 const xprssHB=require('express-handlebars');
 const enRout=require('./controllers');
 const maids=require('./maids/helpers');
-const dataConnect=require('./models/serverConnection');
+const dataConnect=require('./models/sequelCnnctn');
 const SequelizeStore=require('connect-session-sequelize')(session.Store);
 const xprssApp=xprss();
 const CHANNEL=process.env.CHANNEL||4224;
@@ -26,6 +26,7 @@ xprssApp.use(xprss.urlencoded({extended:true}));
 xprssApp.use(xprss.static(pathways.join(__dirname,'public')));
 xprssApp.use(enRout);
 
+//sequelize connection, rename
 dataConnect.sync({force:false}).then(()=>{
     xprssApp.listen(CHANNEL,()=>console.log('live connection to database'));
 })
