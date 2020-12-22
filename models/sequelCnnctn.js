acedec083817d80b87e40d84d97ccd76b443ce27
Denny da Vjncj <daVjncjCode@gmail.com>
2020-12-22 10:55:22 -0500

    updated connection

--- models/sequelCnnctn.js
+++ models/sequelCnnctn.js
@@ -1,11 +1,11 @@
 const Sequelize=require('sequelize');
 require('dotenv').config();
-let sequelize;
+let myDolphin;
 
 if(process.env.JAWSDB_URL){
-    sequelize=new Sequelize(process.env.JAWSDB_URL);
+    myDolphin=new Sequelize(process.env.JAWSDB_URL);
 }else{
-    sequelize=new Sequelize(
+    myDolphin=new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
         process.env.DB_PW,
@@ -16,4 +16,4 @@ if(process.env.JAWSDB_URL){
         }
     );
 }
-module.exports=sequelize;
+module.exports=myDolphin;
