2ad7bbc1ed05536ad721c1e80d78f0c4b32a62b4
Denny da Vjncj <daVjncjCode@gmail.com>
2020-12-23 11:13:45 -0500

    tentative seeding

--- models/fertilizer/granules.js
+++ models/fertilizer/granules.js
@@ -1,1 +1,17 @@
-const connectivity=require('./connectivity/sequelCnnctn')
+const connectivity=require('./connectivity/sequelCnnctn');
+const {Posts,Comments,Articles,User}=require('./models');
+const postData=require('./fertilizer/postData');
+const userData=require('./fertilizer/userData');
+const cmmntData=require('./fertilizer/cmmentData');
+const artcleData=require('./fertilizer/artcleData');
+
+const feedDatabase=async()=>{
+    await connectivity.sync({force:true});
+
+    const users=await User.bulkCreate(userData,{
+        individualHooks:true,
+        returning:true,
+    });//we'll have multiple users
+    process.exit(0)
+};
+feedDatabase();
