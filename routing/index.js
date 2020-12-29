16916f396d90a8420f45cf67afe3b2d534a5e1aa
Denny da Vjncj <daVjncjCode@gmail.com>
2020-12-28 21:56:47 -0500

    configuring rout index

--- routing/index.js
+++ routing/index.js
@@ -1,1 +1,5 @@
 //BUILD OUT ALL ROUTES IN ORDER TO RENDER
+const compass=require('express').Router();
+const theWayHome=require('./homeRout');
+compass.use('/',theWayHome);
+    module.exports=compass;
