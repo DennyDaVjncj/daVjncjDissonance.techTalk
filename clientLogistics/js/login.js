88753411b7656d83f50541534c974f78221589b6
Denny da Vjncj <daVjncjCode@gmail.com>
2021-01-10 19:57:50 -0500

    folder update

--- clientLogistics/js/login.js
+++ clientLogistics/js/login.js
@@ -5,14 +5,15 @@ const autonomousLogic=async UX=>{
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
-    if (email && password) {
-      // Send a POST request to the API endpoint
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
+    // if (email && password) {
+    //   // Send a POST request to the API endpoint
+    //   const data = await fetch('/api/users/login', {
+    //     method: 'POST',
+    //     body: JSON.stringify({ email, password }),
+    //     headers: { 'Content-Type': 'application/json' },
+    //   });
+      
+
       if (response.ok) {
         // If successful, redirect the browser to the profile page
         document.location.replace('/dashboard');
