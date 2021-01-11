76370824ee9e250aba569d1956c24b1af34b5878
Denny da Vjncj <daVjncjCode@gmail.com>
2021-01-11 16:31:59 -0500

    updated iFace logic

--- clientLogistics/js/login.js
+++ clientLogistics/js/login.js
@@ -1,4 +1,4 @@
-const autonomousLogic=async UX=>{
+const interfaceLogic=async UX=>{
   UX.preventDefault();
 
     // Collect values from the login form
@@ -43,7 +43,7 @@ const autonomousLogic=async UX=>{
   
   document
     .querySelector('.login-form')
-    .addEventListener('submit', autonomousLogic);
+    .addEventListener('submit', interfaceLogic);
   
   document
     .querySelector('.signup-form')
