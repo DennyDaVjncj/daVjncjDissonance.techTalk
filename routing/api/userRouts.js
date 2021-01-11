026d6bf1c557ae5c9723558e1d895c35de8d8f47
Denny da Vjncj <daVjncjCode@gmail.com>
2021-01-11 10:38:34 -0500

    update to userRout

--- routing/api/userRouts.js
+++ routing/api/userRouts.js
@@ -1,2 +1,16 @@
 const compass=require('express').Router();
 const {Blogs,User}=require('../../models');
+const daVjncjScan=require('../../virtualAssist/daVjncjScan');
+
+compass.post('/',async(ask,echo)=>{
+    try{
+        const newUser=await User.create({
+            ...ask.body,
+        })
+        console.log(newUser);
+        echo.status(200).json(newUser);
+    }catch(typo){
+        echo.status(500)
+    }
+})
+module.exports=compass;
